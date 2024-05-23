8021dc65eeae38ba90949a6936dc16303097715c
macs1756 <kindrativmaksum312@gmail.com>
2024-05-23 22:54:04 +0300

    v2

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const { knex } = require('./knex')
-
-
+const { knex } = require('./knex');
 
 const getProducts = async () => {
   try {
-    const products = await knex('products').select('id').limit(10);
-    console.log(products);
+    const products = await knex('products').select('id', 'description').limit(10);
+    return products.map(e => e.description && (`<section><div>${e.id + " " +e.description}</div><div>_</div></section>`)).join('');
   } catch (error) {
     console.error('Error fetching products:', error);
   } finally {
@@ -13,4 +11,20 @@ const getProducts = async () => {
   }
 };
 
-getProducts();
+const fetchProductsHtml = async () => {
+  const productsHtml = await getProducts();
+  return productsHtml;
+};
+
+const http = require('http');
+
+const server = http.createServer(async (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  const productsHtml = await fetchProductsHtml();
+  res.write(productsHtml);
+  res.end();
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
--- knex.js
+++ knex.js
@@ -1,10 +1,14 @@
+
+
  const knex = require('knex')({
   client: 'pg',
   connection: {
     connectionString: 'postgres://postgres:admin@127.0.0.1:4444/vapping360',
     ssl: false,
   },
-});
+ });
+
+ 
 
 module.exports = {
   knex
