b98a67d0214124001f4ff07f2d3962c902d8fdfc
macs1756 <kindrativmaksum312@gmail.com>
2024-05-27 08:20:12 +0300

    v8.19

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const { knex } = require('./knex');
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 const getProducts = async () => {
   try {
     const products = await knex('products').select('id', 'description').limit(10);
-    return products.map(e => e.description && (`<section><div>${e.id + " " +e.description}</div><div>_</div></section>`)).join('');
+    return products.map(e => e.description && (`<section><div>${e.id + " " + e.description}</div><div>_</div></section>`)).join('');
   } catch (error) {
     console.error('Error fetching products:', error);
   } finally {
@@ -16,15 +25,19 @@ const fetchProductsHtml = async () => {
   return productsHtml;
 };
 
-const http = require('http');
 
-const server = http.createServer(async (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  const productsHtml = await fetchProductsHtml();
-  res.write(productsHtml);
-  res.end();
+app.get('/', async (req, res) => {
+  try {
+    const html = await fetchProductsHtml();
+    res.send(html);
+  } catch (error) {
+    console.error('Error fetching product HTML:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-server.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
