4ea85ba0cdb74141bfb3d04b74bb1b5866479ee3
macs1756 <kindrativmaksum312@gmail.com>
2024-05-27 08:29:59 +0300

    v8.29

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { knex } = require('./knex');
+
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -9,31 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-const getProducts = async () => {
-  try {
-    const products = await knex('products').select('id', 'description').limit(10);
-    return products.map(e => e.description && (`<section><div>${e.id + " " + e.description}</div><div>_</div></section>`)).join('');
-  } catch (error) {
-    console.error('Error fetching products:', error);
-  } finally {
-    knex.destroy();
-  }
-};
-
-const fetchProductsHtml = async () => {
-  const productsHtml = await getProducts();
-  return productsHtml;
-};
 
 
-app.get('/', async (req, res) => {
-  try {
-    const html = await fetchProductsHtml();
-    res.send(html);
-  } catch (error) {
-    console.error('Error fetching product HTML:', error);
-    res.status(500).send('Internal Server Error');
-  }
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'template', 'index.html'));
 });
 
 
