d4e27ed6fb05689a0928b1525640714326d9cd39
macs1756 <kindrativmaksum312@gmail.com>
2024-06-04 08:45:11 +0300

    8:44

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 const getProducts = require('./server/getProducts.js');
-
+const getProductsById = require('./server/getProductById')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,8 +16,9 @@ app.get('/', (req, res) => {
 });
 
 
-app.use('/products', getProducts);
+app.use('/api/products', getProducts);
 
+app.use('/api/products/:id', getProductsById)
 
 
 app.listen(port, () => {
---
+++ server/getProductById.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { knex } = require('../knex');
+const router = express.Router();
+
+const getProductsById = async (id) => {
+  try {
+    const products = await knex("products")
+      .select("id", "name")
+      .where({ id })
+      .limit(20);
+    return products
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
+};
+
+router.get('/', async (req, res) => {
+  res.json(await getProductsById(req.params.id));
+});
+
+module.exports = router;
